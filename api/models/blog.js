a45f55f53744c90fbce3b01a5c3731468b49a55e
Zulaika <zulaika.ali@outlook.com>
2022-09-14 11:34:19 +0100

    add blog model js

---
+++ api/models/blog.js
@@ -0,0 +1,47 @@
+const db = require('../dbConfig')
+
+class blogEntry {
+    constructor(data){
+        this.is = data.id
+        this.name = data.name
+        this.title = data.title
+        this.text = data.text
+    }
+
+    static get all () {
+        return new Promise (async(resolve,reject) => {
+            try {
+                const blogData = await db.query(`SELECT * FROM blogs`)
+                const blog = 
+                resolve(blogs);
+            } catch (err){
+                reject("Error retrieving blogs")
+            }
+        })
+    }
+
+    static findById (id) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let blogData = await db.query(`SELECT * FROM blogs WHERE id = $1;`, [ id ]);
+                let blog = new blogEntry(blogData.rows[0]);
+                resolve (blog);
+            } catch (err) {
+                reject('Blog was not found');
+            }
+        });
+    }
+
+    static create(name,title,text){
+        return new Promise (async (resolve, reject) => {
+            try {
+                let blogData = await db.query(`INSERT INTO blog (name, title,text) VALUES ($1, $2, $3) RETURNING *;`, [ name,title,text ]);
+                let newBlog = new blogEntry(blogData.rows[0]);
+                resolve (newBlog);
+            } catch (err) {
+                reject('Error creating blog');
+            }
+        });
+    }
+
+}
--- db/1_tables.sql
+++ db/1_tables.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXSITS blogs;
 
 CREATE TABLE blogs (
     id serial PRIMARY KEY,
-    title varchar(100) NOT NULL,
     name varchar(100) NOT NULL,
+    title varchar(100) NOT NULL,
     text varchar(1000) NOT NULL,
 );
